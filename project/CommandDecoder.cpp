6bd2e6dbf27c5b7151e13bc79c54a08a962ad87f
khosta77 <puwerfulpants@gmail.com>
2024-07-30 18:19:18 +0300

    Перенес и разбил класс на cpp и h

---
+++ project/CommandDecoder.cpp
@@ -0,0 +1,38 @@
+#include "CommandDecoder.h"
+
+std::string server::CommandDecoder::callCMD( const std::string& command, const std::string& content,
+        const size_t& i )
+{
+    if( command == "vx" )
+        return _tvc.call( content, i );
+    if( command== "sample" )
+        return _tsc.call( content, i );
+    if( command == "pulse" )
+        return _tpc.call( content, i );
+    return std::string( ( _prefix + ":UNKHOWN_COMMAND;" ) );
+}
+
+std::pair<bool, size_t> server::CommandDecoder::findPrefix( const std::string& content )
+{
+    for( size_t i = 0; i <= _prefix.size(); ++i )
+        if( content.substr( 0, i ) == _prefix )
+            return std::pair<bool, size_t>( false, ( i + 1 ) );
+    return std::pair<bool, size_t>( true, 0 );
+}
+
+std::string server::CommandDecoder::decode( const std::string& content )
+{
+    auto it = findPrefix(content);
+    if( it.first )
+        return std::string( ( _prefix + ":NO_PREFIX;" ) );
+    std::string command = "";
+    for( size_t i = it.second; i < content.size(); ++i )
+    {
+        if( content[i] == '=' )
+            return callCMD( command, content, ( i + 1 ) );
+        command += content[i];
+        }
+    return std::string( ( _prefix + ":NO_OSC;" ) );
+}
+
+
--- project/CommandDecoder.h
+++ project/CommandDecoder.h
@@ -1,6 +1,35 @@
 #ifndef PROJECT_COMMAND_DECODER_H_
 #define PROJECT_COMMAND_DECODER_H_
 
+#include "TypeVCommands.h"
+#include "TypeSCommands.h"
+#include "TypePCommands.h"
 
+namespace server
+{
+
+    class CommandDecoder
+    {
+    private:
+        const std::string _prefix;
+
+        TypeVCommands _tvc;
+        TypeSCommands _tsc;
+        TypePCommands _tpc;
+
+        std::string callCMD( const std::string& command, const std::string& content, const size_t& i );
+        std::pair<bool, size_t> findPrefix( const std::string& content );
+
+    public:
+        CommandDecoder( const std::string prefix, oscilloscopes::Oscilloscope *osc ) : _prefix(prefix),
+            _tvc( osc, _prefix, "vx" ), _tsc( osc, _prefix, "scale" ), _tpc( osc, _prefix, "pulse" ) {}
+
+        ~CommandDecoder() = default;
+
+        std::string decode( const std::string& content );
+
+    };  // CommandDecoder
+
+};  // server
 
 #endif  // PROJECT_COMMAND_DECODER_H_
