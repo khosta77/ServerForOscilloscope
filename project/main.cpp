4cc7a16ddeaecc52a61da00331ecb70407917b7b
khosta77 <puwerfulpants@gmail.com>
2024-08-05 15:48:30 +0300

    Вернул заглушку

--- project/main.cpp
+++ project/main.cpp
@@ -8,8 +8,6 @@ int main( int argc, char* argv[] )
     //oscilloscopes::hantek::Hantek6022 oscilloscope;
     oscilloscopes::plug::OscPlug oscilloscope;
     auto arg = readArg( argc, argv );
-    if( std::get<0>(arg) == "" )
-        return -1;
     server::CommandDecoder cmdec( std::get<0>(arg), &oscilloscope );
     server::Server server( std::get<1>(arg), std::get<2>(arg), &cmdec );
     return server.run();
