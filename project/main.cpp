8ac466dabc70b6de2eeb8c4efc164ba083d8cb31
khosta77 <puwerfulpants@gmail.com>
2024-09-09 23:49:31 +0300

    Переключил на заглушку

--- project/main.cpp
+++ project/main.cpp
@@ -1,12 +1,12 @@
-#include "../HT6022_lib_cpp/project/hantek6022.h"
-//#include "../oscPlug/oscilloscope_plug.h"
+//#include "../HT6022_lib_cpp/project/hantek6022.h"
+#include "../oscPlug/oscilloscope_plug.h"
 #include "Server.h"
 #include "utils.h"
 
 int main( int argc, char* argv[] )
 {
-    oscilloscopes::hantek::Hantek6022 oscilloscope;
-    //oscilloscopes::plug::OscPlug oscilloscope;
+    //oscilloscopes::hantek::Hantek6022 oscilloscope;
+    oscilloscopes::plug::OscPlug oscilloscope;
     auto arg = readArg( argc, argv );
     server::CommandDecoder cmdec( std::get<0>(arg), &oscilloscope );
     server::Server server( std::get<1>(arg), std::get<2>(arg), &cmdec );
